b074dac17912155d6a908b059fb9905228c9c7fc
杨亮 <kumashisei@yangliangdeimac.local>
2017-10-20 09:09:01 +0800

    first

---
+++ JHLDManager/JHLDManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHLDManager
+//
+//  Created by 杨亮 on 2017/10/20.
+//  Copyright © 2017年 booway.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
